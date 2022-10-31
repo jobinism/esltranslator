5a16ab4854d3a8d357ffb5a66864d16403a73ac5
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-10-31 12:26:34 -0600

    added react-speech-recognition package, imported components from Google Clone and from RSR docs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import useApplicationData from './Hooks/useEffect'
+import SpeechToText from '../src/components/SpeechToText'
 const App = () => {
   const {
       state,
@@ -8,11 +9,13 @@ const App = () => {
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
 return (<div className="App" >
+  <SpeechToText />
   <h1> Users </h1>
 
   <ul> {userList} </ul>
 </div >
 );
+
 };
 
 export default App;
