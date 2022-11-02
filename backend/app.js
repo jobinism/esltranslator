008cd1ad6e0f9bff337c23bd61d10cc9a3022811
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-11-02 15:33:04 -0600

    built and routed registration component

--- backend/app.js
+++ backend/app.js
@@ -17,5 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter(dbHelpers));
+ 
 
 module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
 import './App.css';
-import useApplicationData from './Hooks/useEffect'
-import TextField from '../src/components/TextField';
-import Login from './components/Login';
+import { useState, useEffect } from "react";
+import useApplicationData from './Hooks/useEffect';
+// import Translate from './components/translate';
+// import AccountMenu from './components/AccountMenu';
+import Register from './components/Register';
+
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("f", "8khz_8bit_mono");
+encodedParams.append("c", "mp3");
+encodedParams.append("r", "0");
+encodedParams.append("hl", "en-us");
+encodedParams.append("src", "Hello, world!");
+encodedParams.append("b64", true);
+
+const options = {
+  method: 'POST',
+  url: 'https://voicerss-text-to-speech.p.rapidapi.com/',
+  params: {key: 'a9a3766b578a4fc5b1f61e79e32f9430'},
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': '99129ff186msh350496067e563f8p1154adjsnb260d0841d43',
+    'X-RapidAPI-Host': 'voicerss-text-to-speech.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+
 const App = () => {
+  const [aud, setAud] = useState();
+  // const reply = axios.request(options);
+  // reply.then(res => {setAud(res.data)});
+  // console.log(aud);
+  
+
+  const start = () => {
+    document.getElementById('audio').play();
+  }
+  
   const {
       state,
       dispatch
   } = useApplicationData();
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
-return (<div className="App" >
+return (
+<div className="App" >
+  <Register />
   <h1> Users </h1>
-  <Login  />
-  <br />
-  <TextField />
-
+  
   <ul> {userList} </ul>
+      {/* <Translate /> */}
 </div >
 );
 };
 
 export default App;
+
