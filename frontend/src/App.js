959b4766c6dc6d890504ed235d7a053588590993
Simran Kalirai <kaliraisimran@gmail.com>
2022-11-02 22:00:06 -0700

    added popover and language select bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,9 @@
-import React from 'react';
 import './App.css';
-import useApplicationData from './Hooks/useEffect';
 import * as React from 'react';
-import Translate from './components/translate';
-
-
-const encodedParams = new URLSearchParams();
-encodedParams.append("f", "8khz_8bit_mono");
-encodedParams.append("c", "mp3");
-encodedParams.append("r", "0");
-encodedParams.append("hl", "en-us");
-encodedParams.append("src", "Hello, world!");
-encodedParams.append("b64", true);
-
-const options = {
-  method: 'POST',
-  url: 'https://voicerss-text-to-speech.p.rapidapi.com/',
-  params: {key: 'a9a3766b578a4fc5b1f61e79e32f9430'},
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'X-RapidAPI-Key': '99129ff186msh350496067e563f8p1154adjsnb260d0841d43',
-    'X-RapidAPI-Host': 'voicerss-text-to-speech.p.rapidapi.com'
-  },
-  data: encodedParams
-};
-
-
-
+import Translate from './components/Translate';
 import AccountMenu from './components/AccountMenu';
-import useApplicationData from './Hooks/useEffect'
+import useApplicationData from './Hooks/useEffect';
+import Popup from './components/Popover';
 const App = () => {
   const {
       state,
