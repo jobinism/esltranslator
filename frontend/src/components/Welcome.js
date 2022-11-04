0bf43a8e33fcc08e94d47153b3af822574b9561c
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-11-03 22:40:51 -0600

    added multi-language welcome widget, using CSS animation

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Welcome.css';
+
+export default function Welcome() {
+  return (
+    <div>
+      <h1> <span className="welcome"> </span> </h1>
+    </div>
+  );
+}
