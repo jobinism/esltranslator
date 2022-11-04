9b0a7b9a66762991d4639b9e586b62d21b676e3c
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-11-04 15:29:27 -0600

    fixed some mui dependency bugs

---
+++ frontend/src/AboutUs.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => { 
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Some text about us</p>
+    </div>
+  );
+}
+
+export default AboutUs;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Translate from './components/Translate';
 import AccountMenu from './components/AccountMenu';
 import Popup from './components/Popover';
@@ -15,12 +16,12 @@ import Welcome from './components/Welcome';
 
 const App = () => {
   
-  const {
-      state,
-      dispatch
-  } = useApplicationData();
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
-));
+//   const {
+//       state,
+//       dispatch
+//   } = useApplicationData();
+//     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+// ));
 return (
 <div className="App" >
   <Navbar />
