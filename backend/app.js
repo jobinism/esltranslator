f63182bb527725b7f1819a14e0f28b8d88d4449d
Simran Kalirai <kaliraisimran@gmail.com>
2022-11-06 22:57:03 -0800

    added comments to navbar

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const logger = require('morgan');
 const db = require('./db');
 const dbHelpers = require('./helpers/dbHelpers')(db);
 const cors = require('cors');
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const historyRouter = require('./routes/wordHistory');
 const logoutRouter = require('./routes/logout');
